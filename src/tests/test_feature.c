1d484b52f1eb7d5074fb8ecb5253301215198467
Dimitar Krastev <dimi1010100@gmail.com>
2025-09-19 09:23:16 +0300

    Copy changes from pcapplusplus repo.

--- src/tests/test_feature.c
+++ src/tests/test_feature.c
@@ -26,12 +26,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#ifdef _WIN32
-	#include <winsock2.h>
-	#pragma comment(lib, "ws2_32.lib")
-#else
-	#include <arpa/inet.h>
-#endif
+#include <arpa/inet.h>
 
 enum feature_description {
 	FEATURE_PROTOCOL = 0,
--- src/tests/test_read_packets.c
+++ src/tests/test_read_packets.c
@@ -63,7 +63,7 @@ int main(int argc, const char **args) {
 							pkt_header.interface_id,
 							pkt_header.data_link,
 							(int)pkt_header.timestamp.tv_sec,
-							(int)pkt_header.timestamp.tv_nsec);
+							(int)pkt_header.timestamp.tv_usec);
 					if (pkt_header.comment_length > 0)
 						printf(", comment=\"%s\"\n", pkt_header.comment);
 					else
